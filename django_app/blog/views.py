5801b7572ffe67400ee711c298d490e62d387f9e
GAIUS8 <gaius827@gmail.com>
2017-05-30 20:51:41 +0900

    make initial VIEW & TEMPLATEs

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    post = Post.objects.all().order_by('-created_date')
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_list.html', context=context)
