1acb800e303350394aa38fdf25eb6adbc61d8559
GAIUS8 <gaius827@gmail.com>
2017-05-31 01:22:39 +0900

    make post_detail, make post_create

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
 from .models import Post
+from django.http import HttpResponse
+from .Forms import PostCreateForm
+
+User = get_user_model()
 
 
 def post_list(request):
@@ -9,3 +14,38 @@ def post_list(request):
         'post': post,
     }
     return render(request, 'blog/post_list.html', context=context)
+
+
+def post_detail(request, pk):
+    context = {
+        'post': Post.objects.get(pk=pk),
+    }
+    return render(request, 'blog/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'GET':
+        form = PostCreateForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'blog/post_create.html', context)
+    elif request.method == 'POST':
+        form = PostCreateForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            text = form.cleaned_data['text']
+            user = User.objects.first()
+            post = Post.objects.create(
+                title=title,
+                text=text,
+                author=user
+            )
+            return redirect('post_detail', pk=post.pk)
+
+        else:
+            context = {
+                'form': form,
+            }
+            return render(request, 'blog/post_create.html', context)
